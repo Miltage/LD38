e5042280d9b8ed0e347ee5a22ccb92ae021c3633
Robert Shenton <theshenton@gmail.com>
2017-04-22 22:59:21 +0200

    Add soft bodies

--- Source/Battle.hx
+++ Source/Battle.hx
@@ -10,6 +10,9 @@ import nape.space.Broadphase;
 import nape.phys.Body;
 import nape.phys.BodyList;
 import nape.phys.BodyType;
+import nape.phys.Compound;
+import nape.geom.GeomPoly;
+import nape.shape.Edge;
 import nape.shape.Polygon;
 import nape.geom.Vec2;
 import nape.util.Debug;
@@ -19,6 +22,8 @@ import nape.constraint.PivotJoint;
 
 import openfl.Lib;
 
+typedef SoftBody = Compound;
+
 class Battle extends Sprite
 {
 	public static inline var VEL_ITERATIONS:Int = 10;
@@ -26,6 +31,7 @@ class Battle extends Sprite
 
 	private var space:Space;
 	private var prevTime:Int;
+  private var softBodies:Array<SoftBody>;
 
 	public function new()
   {
@@ -38,7 +44,7 @@ class Battle extends Sprite
   {
   	removeEventListener(Event.ADDED_TO_STAGE, start);
 
-  	var gravity = Vec2.weak(0, 600);
+  	var gravity = Vec2.weak(0, 0);
     space = new Space(gravity);
 
     prevTime = Lib.getTimer();
@@ -50,13 +56,19 @@ class Battle extends Sprite
     floor.shapes.add(new Polygon(Polygon.rect(50, (h - 50), (w - 100), 1)));
     floor.space = space;
 
-    for (i in 0...16)
-    {
-      var box = new Body(BodyType.DYNAMIC);
-      box.shapes.add(new Polygon(Polygon.box(16, 32)));
-      box.position.setxy((w / 2), ((h - 50) - 32 * (i + 0.5)));
-      box.space = space;
-    }
+    softBodies = [];
+
+    var poly = new GeomPoly(Polygon.regular(30, 30, 20));
+    var x = 0;
+    var y = 5;
+        var body = polygonalBody(
+            Vec2.get(w/2 + x * 60, h - (y + 0.5) * 60),
+            /*thickness*/ 10, /*discretisation*/ 15,
+            /*frequency*/ 30, /*damping*/ 10,
+            poly
+        );
+        softBodies.push(body);
+        body.space = space;
 
     addEventListener(Event.ENTER_FRAME, onEnterFrame);
   }
@@ -78,56 +90,143 @@ class Battle extends Sprite
     if (deltaTime == 0)
       return;
 
-    
-
     var noStepsNeeded = false;
 
-    //if (variableStep) {
-      if (deltaTime > (1000 / 30)) {
-          deltaTime = (1000 / 30);
-      }
+    if (deltaTime > (1000 / 30))
+    {
+      deltaTime = (1000 / 30);
+    }
 
-      Main.debug.clear();
+    Main.debug.clear();
 
-      preStep(deltaTime * 0.001);
-      if (space != null) {
-          space.step(deltaTime * 0.001, VEL_ITERATIONS, POS_ITERATIONS);
-      }
-      prevTime = curTime;
-    /*}
-    else {
-        var stepSize = (1000 / stage.frameRate);
-        stepSize = 1000/60;
-        var steps = Math.round(deltaTime / stepSize);
+    preStep(deltaTime * 0.001);
+    if (space != null)
+    {
+      space.step(deltaTime * 0.001, VEL_ITERATIONS, POS_ITERATIONS);
+    }
+    prevTime = curTime;
 
-        var delta = Math.round(deltaTime - (steps * stepSize));
-        prevTime = (curTime - delta);
-        if (steps > 4) {
-            steps = 4;
-        }
-        deltaTime = stepSize * steps;
-
-        if (steps == 0) {
-            noStepsNeeded = true;
-        }
-        else {
-            debug.clear();
-        }
-
-        while (steps-- > 0) {
-            preStep(stepSize * 0.001);
-            if (space != null) {
-                space.step(stepSize * 0.001, velIterations, posIterations);
-            }
-        }
-    }*/
-
-    if (space != null && !noStepsNeeded) {
+    if (space != null && !noStepsNeeded)
+    {
       Main.debug.draw(space);
     }
-    if (!noStepsNeeded) {
+    if (!noStepsNeeded)
+    {
       postUpdate(deltaTime * 0.001);
       Main.debug.flush();
     }
+  }
+
+  function polygonalBody(position:Vec2, thickness:Float, discretisation:Float, frequency:Float, damping:Float, poly:GeomPoly):SoftBody
+  {
+    var body = new SoftBody();
+
+    var segments = [];
+    var outerPoints = [];
+    var innerPoints = [];
+    var refEdges = [];
+    body.userData.refEdges = refEdges;
+
+    var inner = poly.inflate(-thickness);
+
+    var start = poly.current();
+    do 
+    {
+      var current = poly.current();
+      poly.skipForward(1);
+      var next = poly.current();
+
+      var iCurrent = inner.current();
+      inner.skipForward(1);
+      var iNext = inner.current();
+
+      var delta = next.sub(current);
+      var iDelta = iNext.sub(iCurrent);
+
+      var length = Math.max(delta.length, iDelta.length);
+      var numSegments = Math.ceil(length / discretisation);
+      var gap = (1 / numSegments);
+
+      for (i in 0...numSegments)
+      {
+        var segment = new Body();
+
+        var outerPoint = current.addMul(delta, gap * i);
+        var innerPoint = iCurrent.addMul(iDelta, gap * i);
+        var polygon = new Polygon([
+            outerPoint,
+            current.addMul(delta, gap * (i + 1), true),
+            iCurrent.addMul(iDelta, gap * (i + 1), true),
+            innerPoint
+        ]);
+        polygon.body = segment;
+        segment.compound = body;
+        segment.align();
+
+        segments.push(segment);
+        outerPoints.push(outerPoint);
+        innerPoints.push(innerPoint);
+
+        refEdges.push(polygon.edges.at(0));
+      }
+
+      delta.dispose();
+      iDelta.dispose();
+    }
+    while (poly.current() != start);
+
+    for (i in 0...segments.length)
+    {
+      var leftSegment = segments[(i - 1 + segments.length) % segments.length];
+      var rightSegment = segments[i];
+
+      var current = outerPoints[i];
+      var pivot = new PivotJoint(
+        leftSegment, rightSegment,
+        leftSegment.worldPointToLocal(current, true),
+        rightSegment.worldPointToLocal(current, true)
+      );
+      current.dispose();
+      pivot.compound = body;
+
+      current = innerPoints[i];
+      pivot = new PivotJoint(
+        leftSegment, rightSegment,
+        leftSegment.worldPointToLocal(current, true),
+        rightSegment.worldPointToLocal(current, true)
+      );
+      current.dispose();
+      pivot.compound = body;
+      pivot.stiff = false;
+      pivot.frequency = frequency;
+      pivot.damping = damping;
+
+      pivot.ignore = true;
+    }
+
+    inner.clear();
+
+    for (s in segments)
+    {
+      s.position.addeq(position);
+    }
+
+    body.userData.area = polygonalArea(body);
+
+    return body;
+  }
+
+  static var areaPoly = new GeomPoly();
+  static function polygonalArea(s:SoftBody):Float
+  {
+    var refEdges:Array<Edge> = s.userData.refEdges;
+    for (edge in refEdges)
+    {
+        areaPoly.push(edge.worldVertex1);
+    }
+    var ret = areaPoly.area();
+    areaPoly.clear();
+
+    return ret;
   }
 }
